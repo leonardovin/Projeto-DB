43dc90437613d7c1f88a6052e394ed9119ee6127
Lucas Xavier Leite <lucasxavierleite@pm.me>
2023-06-21 23:58:14 -0300

    Add create new user functionality

--- app/courses.py
+++ app/courses.py
@@ -1,7 +1,28 @@
+import psycopg2
 from prettytable import from_db_cursor
 
-def list(cursor):
-    cursor.execute('SELECT * FROM Curso;')
+def list(connection, cursor):
+    try:
+        cursor.execute('SELECT * FROM Curso;')
+        connection.commit()
+        print('Cursos:')
+        print(from_db_cursor(cursor))
+    except Exception as e:
+        connection.rollback()
+        print(e)
 
-    print('Cursos:')
-    print(from_db_cursor(cursor))
+def insert_student(connection, cursor, student):
+    insert_user_query = 'INSERT INTO usuario(cpf, nome, email, senha, tipo, data_nasc, endereco, telefone, idioma1, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    insert_student_query = 'INSERT INTO aluno(usuario, assinante) VALUES (%s, %s)';
+
+    try:
+        print(tuple(student.values()))
+        cursor.execute(insert_user_query, tuple(student.values()))
+        cursor.execute(insert_student_query, (student['cpf'], False))
+        connection.commit()
+        print('Usuário inserido com sucesso.')
+    except psycopg2.errors.UniqueViolation:
+        print('Já existe um usuário com esse CPF.')
+    except Exception as e:
+        connection.rollback()
+        print(e)
--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ import courses
 def menu():
     print('''
           1. Listar cursos
+          2. Listar alunos de um curso
+          3. Inserir aluno em um curso
           q. Sair
           ''')
 
@@ -12,7 +14,26 @@ def menu():
 
     match option:
         case '1':
-            courses.list(cursor)
+            courses.list(connection, cursor)
+        case '2':
+            # course = input()
+            # courses.list_students(connection, cursor, course)
+            raise NotImplementedError()
+        case '3':
+            student = {
+                'cpf': '123',
+                'nome': 'A',
+                'email': 'x@y.com',
+                'senha': '123',
+                'tipo': 'ALUNO',
+                'data_nasc': '10/07/1979',
+                'endereco': 'Rua A, 48',
+                'telefone': '(00) 12345-6789',
+                'idioma1': 'Português (Brasil)',
+                'status': 'ATIVO',
+            }
+
+            courses.insert_student(connection, cursor, student)
         case 'q':
             db.close(cursor, connection)
             quit()
