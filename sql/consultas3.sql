cd31d0d65c056823e41c61b3e2b5ade5f779764e
Lucas Xavier Leite <lucasxavierleite@pm.me>
2023-06-28 17:25:30 -0300

    Update select queries

--- sql/consultas3.sql
+++ sql/consultas3.sql
@@ -1,11 +1,30 @@
 -- Selecionar média de avaliação de tutores de cursos que contenham recurso pago vídeo tutorial e sejam cursados por mais de 5 alunos. Selecionar também a média de avaliação dos cursos
+SELECT COUNT(*) AS "Agendamentos", COUNT(DISTINCT agendamento.aluno) AS "Alunos atendidos"
+FROM agendamento
+JOIN especialista ON especialista.tutor = agendamento.especialista
+WHERE agendamento.data_hora >= '2023-01-01' AND agendamento.data_hora <= '2023-12-31'
+GROUP BY agendamento.especialista;
 
-SELECT AVG(ta.avaliacao) AS "Média de Avaliação dos Tutores", AVG(c.media_aval) AS "Média de Avaliação dos Cursos"
-FROM tutor_avaliacao ta
-JOIN tutor t ON ta.tutor = t.usuario
-JOIN voluntario v ON t.usuario = v.tutor
-JOIN tutoria tr ON tr.voluntario = v.tutor
-JOIN curso c ON c.codigo = tr.curso
-JOIN recurso_pago rp ON c.codigo = rp.recurso_curso AND rp.tipo = 'tutoria_personalizada'
-GROUP BY c.codigo
-HAVING COUNT(DISTINCT tr.curso) > 5;
+-- Selecionar média de avaliação de tutores de cursos que contenham recurso pago vídeo tutorial e sejam cursados por mais de 5 alunos. Selecionar também a média de avaliação dos cursos
+
+SELECT AVG(TA.avaliacao) AS "Média de Avaliação dos Tutores", AVG(C.media_aval) AS "Média de Avaliação dos Cursos"
+FROM tutor_avaliacao AS TA
+JOIN tutor AS T ON TA.tutor = t.usuario
+JOIN voluntario AS V ON T.usuario = V.tutor
+JOIN tutoria AS TR ON TR.voluntario = V.tutor
+JOIN curso AS C ON C.codigo = TR.curso
+JOIN aluno_cursa AS AC ON C.codigo = AC.curso
+JOIN recurso_pago AS RP ON C.codigo = RP.recurso_curso AND RP.tipo = 'tutoria_personalizada'
+GROUP BY C.codigo
+HAVING COUNT(AC.aluno) > 5;
+
+-- Selecionar a dificuldade média de todos os cursos, e se houver, média das avaliações dos cursos por categoria, assim como a idade média dos alunos que cursam esses cursos
+
+SELECT C.categoria AS "Categoria", TO_CHAR(AVG(C.nivel_dificuldade), 'FM999999999.00') AS "Dificuldade Média",  TO_CHAR(AVG(AGE(CURRENT_DATE, U.data_nasc)), 'YY') AS "Idade Média", TO_CHAR(AVG(C.media_aval), 'FM999999999.00') AS "Média de Avaliações"
+FROM usuario AS U
+JOIN aluno AS A ON U.cpf = A.usuario
+LEFT JOIN aluno_cursa AS AC ON A.usuario = AC.aluno
+RIGHT JOIN curso as C ON AC.curso = C.codigo
+GROUP BY C.categoria
+HAVING AVG(C.nivel_dificuldade) < 5
+ORDER BY C.categoria;
